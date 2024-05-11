331016eb794d044a851e7bed19dfe15de7e3b35a
Argen Ashimov <retreatpm@icloud.com>
2024-05-11 15:50:17 +0600

    Create tables and models for user and authorities

---
+++ src/main/java/com/example/edufood/model/User.java
@@ -0,0 +1,36 @@
+package com.example.edufood.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@Table(name = "users")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    private String avatar;
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "users", cascade = CascadeType.ALL)
+    private List<Authority> authorities;
+
+}
